60aa9394fbaa329414b7ab8daa93c267d5e0af12
maxoreau <maxime.horeau@gmail.com>
2017-05-24 09:51:06 +0200

    improved Rest java class added static dao that is instanced at startup in static function. The same instance of dao is then used in all methods of the REST service

--- src/main/java/com/maxoreau/restex/rest/RestMax.java
+++ src/main/java/com/maxoreau/restex/rest/RestMax.java
@@ -7,6 +7,7 @@ import javax.ws.rs.DELETE;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,11 +21,15 @@ import com.maxoreau.restex.models.Contact;
 
 @Path("/contacts")
 public class RestMax {
+	private static daoGenerique<Contact> dao;
+	
+	static  {
+		dao = new ContactDaoInDatabase();
+	}
 		
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void createContact(Contact jsonClient) {
-		daoGenerique<Contact> dao = new ContactDaoInDatabase();
 		dao.create(jsonClient);
 	}
 	
@@ -32,29 +37,26 @@ public class RestMax {
 	@Path("{string}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Contact> SearchByString(@PathParam("string") String string) {
-		daoGenerique<Contact> dao = new ContactDaoInDatabase();
 		return dao.readByName(string);
 	}
 		
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Contact> getAllJson() {
-		daoGenerique<Contact> dao = new ContactDaoInDatabase();
 		return dao.getAll();
 			
 	}
 	
-//	@PUT
-//	@Path("{name}")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public void updateContact() {
-//	
-//	}
+	@PUT
+	@Path("{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateContact(Contact contact) {
+		dao.update(contact);
+	}
 	
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void deleteContact(int contactId) {
-		daoGenerique<Contact> dao = new ContactDaoInDatabase();
 		dao.delete(contactId);
 	}
 	
